e6d568a66283121abb77c3f365dce847965a34ab
shubham <shubhshri25gmail.com>
2024-09-01 19:52:26 +0530

    updated cors whitelist

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ const cors = require("cors");
 let whitelist = [
   "https://mern-admin-dash-board-frontend-haxjp9xzs.vercel.app",
   "http://localhost:5173",
+  "https://mern-admin-dash-board-frontend.vercel.app",
 ];
 
 var corsOptions = {
